fa38362d4dc56f241e3377f7d82808168baf51ec
milon19 <milon15-1472@diu.edu.bd>
2020-07-16 00:01:21 +0600

    Create Checkout View

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (CartHome, CartUpdate)
+from .views import (CartHome, CartUpdate, CheckoutView)
 
 urlpatterns = [
     path('', CartHome, name='cart-home'),
     path('update/', CartUpdate, name='cart-update'),
+    path('checkout/', CheckoutView, name='checkout'),
 ]
 
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Cart
 
 from products.models import Product
+from orders.models import Order
 
 
 def CartHome(request):
@@ -28,3 +29,16 @@ def CartUpdate(request):
             redirect('cart-home')
         # cart_obj.products.add(product_id)
     return redirect('cart-home')
+
+
+def CheckoutView(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count()==0:
+        return redirect('cart-home')
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    context = {
+        'object': order_obj
+    }
+    return render(request, 'carts/checkout.html', context)
