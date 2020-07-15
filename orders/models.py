3810ce275d07fbcf38d3affe5732183130123896
milon19 <milon15-1472@diu.edu.bd>
2020-07-15 21:03:21 +0600

    Create Order Model

---
+++ orders/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from carts.models import Cart
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_order_id_generator
+
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+)
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(max_length=120, choices=ORDER_STATUS_CHOICES)
+    shipping_total = models.DecimalField(default=9.99, max_digits=100, decimal_places=2)
+    total = models.DecimalField(default=0, max_digits=100, decimal_places=2)
+
+    def update_total(self):
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
+
+    def __str__(self):
+        return self.order_id
+
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
+
+
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:
+        cart_obj = instance
+        cart_total = cart_obj.total
+        cart_id = cart_obj.id
+        qs = Order.objects.filter(cart__id = cart_id)
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.update_total()
+
+post_save.connect(post_save_order, sender=Order)
