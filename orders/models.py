35d5e54780c2e7e77978401a65570709e891186c
milon19 <milon15-1472@diu.edu.bd>
2020-08-12 18:13:07 +0600

    Finalize Checkout

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ ORDER_STATUS_CHOICES = (
 class OrderManager(models.Manager):
     def new_or_get(self, billing_profile, cart_obj):
         created = False
-        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True)
+        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True, status='created')
         if qs.count() == 1:
             obj = qs.first()
         else:
@@ -33,7 +33,7 @@ class Order(models.Model):
     billing_address = models.ForeignKey(Address, related_name='billing_address', null=True, blank=True, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=120, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    status = models.CharField(max_length=120, choices=ORDER_STATUS_CHOICES)
+    status = models.CharField(max_length=120, choices=ORDER_STATUS_CHOICES, default='created')
     shipping_total = models.DecimalField(default=9.99, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0, max_digits=100, decimal_places=2)
     active = models.BooleanField(default=True)
@@ -48,6 +48,21 @@ class Order(models.Model):
         self.total = formatted_total
         self.save()
         return new_total
+
+    def check_done(self):
+        billing_profile = self.billing_profile
+        shipping_address = self.shipping_address
+        billing_address = self.billing_address
+        total = self.total
+        if billing_profile and shipping_address and billing_address and total>0:
+            return True
+        return False
+
+    def mark_paid(self):
+        if self.check_done():
+            self.status = 'paid'
+            self.save()
+        return self.status
 
     def __str__(self):
         return self.order_id
