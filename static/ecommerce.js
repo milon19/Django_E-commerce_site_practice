d9236b03a929c1c7f3f686630b7e424ef846e0c2
milon19 <milon15-1472@diu.edu.bd>
2020-08-23 01:20:55 +0600

    Add CSRF for AJAX

--- static/ecommerce.js
+++ static/ecommerce.js
@@ -113,7 +113,7 @@ $(document).ready(function (){
                 var navbarCount = $('.navbar-cart-count')
                 navbarCount.text(data.cartItemCount)
                 var currentPath = window.location.href
-                if(currentPath.indexOf('cart') != -1){
+                if(currentPath.indexOf('cart') !== -1){
                     refreshCart()
                 }
             },
