ef2ce4f8a6b72d99af4ff898ce45cf10e39ccd7f
milon19 <milon15-1472@diu.edu.bd>
2020-08-25 23:46:44 +0600

    Modify ProductDetailView for signals

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from .models import Product
 from carts.models import Cart
+from analytics.signals import object_viewed_signal
+
 
 class ProductListView(ListView):
     model = Product
@@ -24,3 +27,20 @@ class ProductDetailView(DetailView):
         cart_obj, new_obj = Cart.objects.new_or_get(self.request)
         context['cart'] = cart_obj
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404('Not found..')
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404('Uhhhmmmu')
+
+        object_viewed_signal.send(instance.__class__, instance=instance, request=request)
+        return instance
